483a3c8b513dbaf375bc09ceba391da0f4b7bcb4
Mara Manuel <maramnuel68@gmail.com.com>
2022-11-01 20:57:52 +0100

    aprendendo styled components

---
+++ src/components/button/Button.styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+ const StyledButton = styled.button`
+cursor:pointer;
+transition: 0.5s all ease-out;
+text-decoration:none;
+border : none;
+box-shadow: 0px 3px 6px rgba(0, 0, 0, 16);
+background-color: white;
+text-align:center;
+padding:10px 40px 10px 40px;
+margin-top:20px
+`
+
+export default StyledButton
